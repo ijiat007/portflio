24129a8d809b1843bcc7532234bc459230f0cfec
ijiat007 <ijiat007@gmail.com>
2022-04-13 16:43:37 +0900

    set

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  outputDir: './docs',
+  publicPath: '/portflio/',
+  transpileDependencies: true,
+  css: {
+    extract: {
+      filename: 'style.css?v=[hash:8]',
+      chunkFilename: 'css/[name].css'
+    },
+    sourceMap: true,
+    loaderOptions: {
+      scss: {
+        // additionalData: '@import "~@/assets/scss/_variables.scss"; @import "~@/assets/scss/_mixin.scss";'
+        additionalData: `
+          @import "@/assets/scss/_variables.scss";
+          @import "@/assets/scss/_mixin.scss";
+        `
+      }
+    }
+  },
 })
