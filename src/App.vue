f189ef6ed8d8b176c80a660c40052d4210aeaaa3
ijiat007 <ijiat007@gmail.com>
2022-04-26 19:43:31 +0900

    Update layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  my portflio
+  <header :class="{ fix: isFix }">
+      <h1>JUYOUNG SUNG</h1>
+      <ul class="nav" v-scroll-spy-active v-scroll-spy-link>
+      <li>
+        <a>Menu 1</a>
+      </li>
+      <li>
+        <a>Menu 2</a>
+      </li>
+    </ul>
+    </header>
+
+    <main v-scroll-spy>
+      <div>
+        11111
+      </div>
+      <div>
+        22222
+      </div>
+    </main>
+
+    <footer>© juyoung.sung. All Rights Reserved.</footer>
 </template>
 
 <script>
@@ -8,9 +29,36 @@
 export default {
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+      isFix: false,
+    }
+  },
+
+  mounted() {
+    // window.addEventListener('scroll', this.onScroll);
+  },
+  methods: {
+    // onScroll() {
+    //   const scrollTop = window.scrollY;
+    //   const nav = document.getElementById("nav");
+    //   const topPos = nav.offsetTop;
+    //   setTimeout(() => {
+    //     this.isFix = scrollTop > topPos
+    //   }, 100);
+    //   console.log(topPos, scrollTop);
+    // },
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+main {
+
+  > div {
+    height: 100vh;
+    border: 1px solid red;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { registerScrollSpy } from 'vue3-scroll-spy';
 import '@/assets/scss/style.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+registerScrollSpy(app);
+
+app.mount('#app');
